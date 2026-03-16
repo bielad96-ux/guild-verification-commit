31084ec3bf3c030626eadcfbdd08c80c74b3ed71
bielad96-ux <bielad96@gmail.com>
2026-03-16 21:12:14 +0100

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-16T20:11:48.105Z
-Commit: Add multi-language support
+Timestamp: 2026-03-16T20:12:07.993Z
+Commit: Resolve null pointer exception in user service
