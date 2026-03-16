06703ec01c9ebe36614309557c8c43ec56b48dbf
bielad96-ux <bielad96@gmail.com>
2026-03-16 21:13:20 +0100

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-16T20:13:10.773Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-16T20:13:13.939Z
+Commit: Fix CSS styling bug on mobile devices
