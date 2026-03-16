88f8b389109de483eefc5a17643d1e73fe37aa49
bielad96-ux <bielad96@gmail.com>
2026-03-16 21:14:37 +0100

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-16T20:14:12.566Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-16T20:14:30.602Z
+Commit: Fix memory leak in event handlers
